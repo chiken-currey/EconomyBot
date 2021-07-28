8865216ae89abc3e0277bd63f998b5f27f42a4c4
chiken-currey <84462735+chiken-currey@users.noreply.github.com>
2021-07-28 10:18:52 -0700

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "ODY5OTkwOTc3NDUwOTY3MTMx.YQGQPA.5Ox0E6O7eDzZfg9nI3_FFFiq228",
+    prefix: "$",
     admins: [
         "PEOPLE WHO CAN USE ADD MONEY (IDS)"
 ],
